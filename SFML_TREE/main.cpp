52730382f7c9763463556736191c1174f51bb52e
xdawxd <xrealz110@gmail.com>
2021-01-02 22:47:44 +0100

    Added Christmas Tree ConvexShape.

--- SFML_TREE/main.cpp
+++ SFML_TREE/main.cpp
@@ -1,55 +1,105 @@
-#include "SFML/Graphics.hpp"
-#include "SFML/Graphics/Color.hpp"
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <iostream>
 
 using namespace sf;
 
-int width = 800, height = 600;
-
 struct Scene
 {
-    sf::RectangleShape trunk;
+    RectangleShape trunk;
+    ConvexShape christmasTree;
 };
 
-Scene createScene()
+Scene createScene(int width, int height)
 {
     Scene rv;
 
-    rv.trunk.setSize(sf::Vector2f(60, 50));
-    rv.trunk.setFillColor(sf::Color(101, 67, 33));
-    rv.trunk.setRotation(90.f);
+    float trunkCenter = (float)width / 2;
+    float trunkHeight = (float)height - 60;
 
-    rv.trunk.setPosition(width/2, height-60);
+    rv.trunk.setSize(Vector2f(50, 60));
+    rv.trunk.setFillColor(Color(71, 54, 24)); // 101, 67, 33
+    rv.trunk.setPosition(trunkCenter-25, trunkHeight);
+
+    //-------------------------CHRISTMAS TREE MODEL-----------------------------//
+
+    rv.christmasTree.setPointCount(19);
+    rv.christmasTree.setFillColor(Color(36, 92, 36));
+    
+    double treeDiv = 200;
+    double treeDec = 235;
+
+    float verticalSpread = 60;
+    float currentHeight = trunkHeight;
+
+    for (int i = 0; i < 19; i++)
+    {
+        if (i <= 9)
+        {
+            if (i == 9)
+            {
+                rv.christmasTree.setPoint(i, Vector2f(trunkCenter, currentHeight - verticalSpread));
+            }
+            else if (i % 2 != 0)
+            {
+                currentHeight = currentHeight - verticalSpread;
+                treeDiv = treeDiv / 1.49;
+                rv.christmasTree.setPoint(i, Vector2f(trunkCenter - (float)treeDiv, currentHeight));
+            }
+            else if (i % 2 == 0)
+            {
+                treeDec = treeDec - 35;
+                rv.christmasTree.setPoint(i, Vector2f(trunkCenter - (float)treeDec, currentHeight));
+            }
+        }
+        else if (i > 9)
+        {
+            if (i % 2 != 0)
+            {
+                rv.christmasTree.setPoint(i, Vector2f(trunkCenter + (float)treeDiv, currentHeight));
+                currentHeight = currentHeight + verticalSpread;
+                treeDiv = treeDiv * 1.49;
+            }
+            else if (i % 2 == 0)
+            {
+                rv.christmasTree.setPoint(i, Vector2f(trunkCenter + (float)treeDec, currentHeight));
+                treeDec = treeDec + 35;
+            }
+        }
+    }
 
     return rv;
 }
 
-void drawScene(sf::RenderWindow& win, const Scene& scene)
+void drawScene(RenderWindow& win, const Scene& scene)
 {
     win.draw(scene.trunk);
+    win.draw(scene.christmasTree);
 }
 /*
 void updateScene(Scene& scene)
 {
-    
+    scene.trunk.move(0.f, -0.9f);
 }
 */
 int main()
 {
-    RenderWindow window(sf::VideoMode(width, height),"Merry Christmas!");
-    
-    Scene sc = createScene();
+    RenderWindow window(VideoMode(800, 600),"Merry Christmas!");
+    window.setFramerateLimit(60);
+
+    Scene sc = createScene(window.getSize().x, window.getSize().y);
 
     while(window.isOpen())
     {
-        sf::Event event;
+        Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == Event::Closed)
                 window.close();
         }
 
         //updateScene(sc);
-
+        
         window.clear();
 
         drawScene(window, sc);
